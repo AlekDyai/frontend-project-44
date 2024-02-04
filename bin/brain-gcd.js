a3a97600f050be64d06265950f265b31aeba1647
AlekDyai <alekdyai@gmail.com>
2024-02-04 15:53:38 +0300

    7.1

---
+++ bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import welcome from '../src/games/index.js';
+import playGcd from '../src/games/gcd.js';
+
+welcome();
+playGcd();
---
+++ src/games/gcd.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+import welcome from './index.js';
+
+export default playGcd;
